41d91ef16eb90d69efbe86f78ef090c991251f58
Edward Omopariola <femiedjr@gmail.com>
2024-07-24 11:44:40 -0400

    another change made on html and javascript

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function getLink() {
   const hrefLink = document.getElementById("name").getAttribute("href"); // Using the ID element
-  document.getElementById("demo").innerHTML = "the link above will take you to the website";
+  document.getElementById("demo").innerHTML = "the link above will take you to google webpage";
 }
